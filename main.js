c9be6f0919dc74c8ef8595f5adf40f0bdefb9e4b
Ajusa Jain <arhamjain@gmail.com>
2016-01-17 10:31:38 -0500

    Removed logging

--- main.js
+++ main.js
@@ -8,24 +8,19 @@ io.on('connection', function(socket) {
         socket.broadcast.emit('newPlayer', data);
         socket.emit('dump', players);
         players.push(data);
-        
-        
     });
     socket.on('update', function(data) {
         data.id = socket.conn.id;
-        console.log(data)
         socket.broadcast.emit('update', data);
-        
+
     });
     socket.on('disconnect', function() {
         for (var i = players.length - 1; i >= 0; i--) {
-        	if(players[i].id == socket.id){
-        		scoket.broadcast.emit('death', socket.id)
-        		players.splice(i);
-        	}
+            if (players[i].id == socket.id) {
+                scoket.broadcast.emit('death', socket.id)
+                players.splice(i);
+            }
         };
     });
 });
-
-
 io.listen(3000);
